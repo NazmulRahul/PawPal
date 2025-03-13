25c4b81e598a2e97150d108802c259a4b4535177
Ajoad Islam Nishat <ajoadislam@gmail.com>
2025-03-13 19:33:25 +0600

    blog segment

--- client/src/Components/Home/BlogCardSegment.jsx
+++ client/src/Components/Home/BlogCardSegment.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import BlogCard from './BlogCard'
+import BlogSide from '../../assets/blogSide.png'
 
 const BlogCardSegment = () => {
   return (
-    <div className='w-full min-h-screen  flex justify-center'>
-        <div className="w-[40%] h-full bg-red-500"></div>
+    <div className='w-full min-h-screen  flex justify-center bg-white'>
+        <div className="w-[40%] h-full p-10 overflow-hidden">
+            <img
+                className="float-left w-[70%] h-auto object-contain rounded-md mr-4 mb-2"
+                src={BlogSide}
+                alt="Blog Image"
+            />
+        </div>
+
         <div className="w-[60%] h-full  flex justify-center items-center">
             <div className="w-[90%] h-[60%]  flex ">
                 <div className="w-[70%] h-full flex flex-wrap gap-[10px]">
