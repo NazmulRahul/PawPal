529840e62f987e4bf2dfdbfa7a8be16ae2983fcb
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2025-03-17 03:03:39 +0600

    typescript configured

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-import express from "express";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = require("express");
+var app = (0, express_1.default)();
+var PORT = 5000;
+// Middleware
+app.use(express_1.default.json());
+// Routes
+app.get("/", function (req, res) {
+    res.send("Hello, TypeScript with Express!");
+});
+// Start Server
+app.listen(PORT, function () {
+    console.log("Server running on http://localhost:".concat(PORT));
+});
