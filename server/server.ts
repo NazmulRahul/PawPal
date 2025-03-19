ddc739375e5ebb835ae592aaa5ee2eb8a14768f0
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2025-03-19 20:22:36 +0600

    test

--- server/server.ts
+++ server/server.ts
@@ -1,18 +1,28 @@
 import express, { Request, Response } from "express";
+import config from 'config'
+import log from "./src/utils/logger";
+import connectDB from "./src/utils/dbConnect";
+
 
 const app = express();
-const PORT = 5000;
+const PORT=config.get<number>('PORT')
+const dbUri=config.get<string>('dbUri')
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript with Express!");
+  res.send("Hello");
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, async() => {
+  log.info(`Server is running on port ${PORT}`);
+  try{
+    await connectDB(dbUri)
+  }catch(e){
+    log.error(e)
+  }
 });
 
