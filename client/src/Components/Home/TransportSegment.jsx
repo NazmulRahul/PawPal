fb63536efb2553f82d8b68beae82b68f39fa6ce5
Ajoad Islam Nishat <ajoadislam@gmail.com>
2025-03-21 00:26:56 +0600

    home adopt segment

--- client/src/Components/Home/TransportSegment.jsx
+++ client/src/Components/Home/TransportSegment.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TransportHero from './TransportHero'
+import TransportDetailsCard from './TransportDetailsCard'
 
 const TransportSegment = () => {
   return (
@@ -8,8 +9,8 @@ const TransportSegment = () => {
       <div className="flex-1 h-full flex flex-col justify-start items-center pt-[10%] gloria-hallelujah-regular">
         <p className='text-2xl font-bold'>Because Every Pet Deserves a Smooth Ride Home</p>
         <p className='text-xl'>&</p>
-        <p className='text-xl font-bold'>How we do it</p>
-        <div className="w-[40%] h-[500px] bg-amber-400 mt-[30px]"></div>
+        <p className='text-xl font-bold'>Here how we do it</p>
+        <TransportDetailsCard/>
       </div>
     </div>
   )
