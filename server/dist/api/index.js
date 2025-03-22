20933ee5d4111cd3aa779a232e52c9990f7fa7eb
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2025-03-22 07:57:05 +0600

    test

--- server/dist/api/index.js
+++ server/dist/api/index.js
@@ -14,10 +14,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const config_1 = __importDefault(require("config"));
-const logger_1 = __importDefault(require("./src/utils/logger"));
-const dbConnect_1 = __importDefault(require("./src/utils/dbConnect"));
+const logger_1 = __importDefault(require("./utils/logger"));
+const dbConnect_1 = __importDefault(require("./utils/dbConnect"));
 const cors_1 = __importDefault(require("cors"));
-const user_route_1 = __importDefault(require("./src/routes/user.route"));
+const user_route_1 = __importDefault(require("./routes/user.route"));
 const app = (0, express_1.default)();
 const PORT = config_1.default.get('PORT');
 const dbUri = config_1.default.get('dbUri');
