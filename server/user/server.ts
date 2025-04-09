7e3fd3c9863539ef009b2f73aed534672107dba1
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2025-04-10 00:53:37 +0600

    test

--- server/user/server.ts
+++ server/user/server.ts
@@ -12,12 +12,12 @@ import swaggerUi from 'swagger-ui-express';
 dotenv.config()
 
 const app = express();
-const PORT=config.get<number>('PORT')
-const dbUri=config.get<string>('dbUri')
-const ip=config.get<string>('ip')
-const corsOption={
-  origin:"*",
-  methods: ["GET", "POST"],    
+const PORT = config.get<number>('PORT')
+const dbUri = config.get<string>('dbUri')
+const ip = config.get<string>('ip')
+const corsOption = {
+  origin: "*",
+  methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }
 
@@ -32,11 +32,11 @@ const swaggerOptions = {
     },
     servers: [
       {
-        url: `http://${ip}:3000/api/user`, 
+        url: `http://${ip}:5000/user/api`,
       },
     ],
   },
-  apis: ['./src/routes/user.route.ts'], 
+  apis: ['./src/routes/user.route.ts'],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
@@ -51,14 +51,17 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello");
 });
 
-app.use('/api/user',userRoutes)
-
+//
+app.use('/api', userRoutes)
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 // Start Server
-app.listen(PORT,ip, async() => {
+app.listen(PORT, ip, async () => {
   log.info(`Server is running on port ${PORT}`);
-  try{
+  try {
     await connectDB(dbUri)
-  }catch(e){
+  } catch (e) {
     log.error(e)
   }
 });
