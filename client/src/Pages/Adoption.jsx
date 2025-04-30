dd3f681f9185be850d420c1976360ece3770c9b0
pepe-le-pewploo <mamun.muntasir003@gmail.com>
2025-05-01 00:28:14 +0600

    Ajaira commit

--- client/src/Pages/Adoption.jsx
+++ client/src/Pages/Adoption.jsx
@@ -19,7 +19,7 @@ const Adoption = () => {
       dispatch(setScrollY(scrollY))
     }
   return (
-    <div ref={scrollRef} onScroll={handleScroll} className="flex flex-col h-full w-full overflow-y-scroll scrollbar-hidden bg-[#EBE8DB] px-14">
+    <div ref={scrollRef} onScroll={handleScroll} className="flex flex-col h-full w-full overflow-y-scroll scrollbar-hidden bg-[#EBE8DB] px-14 min-2xl:px-60">
       <AdoptionLayout/>
     </div>
   )
