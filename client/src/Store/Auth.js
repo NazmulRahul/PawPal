c203b78c6bd096465e336e9fcf1e47bb4e832aa4
Nazmul Hossen Rahul <69730793+nazmul-2019331047@users.noreply.github.com>
2025-06-07 09:29:44 +0600

    test

--- client/src/Store/Auth.js
+++ client/src/Store/Auth.js
@@ -1,83 +1,83 @@
 // import axios from '../Utils/axios';
-import axios from 'axios';
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const login = createAsyncThunk('auth/login', async (data, thunkAPI) => {
-  try {
-    const response = await axios.post(
-      'https://www.pawpalbd.com/api/user/api/login',
-      data
-    );
-    console.log(response);
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(
-      error.response?.data || { message: 'Login failed' }
-    );
-  }
+export const login = createAsyncThunk("auth/login", async (data, thunkAPI) => {
+    try {
+        const response = await axios.post(
+            "https://www.pawpalbd.com/api/user/api/login",
+            data
+        );
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        return thunkAPI.rejectWithValue(
+            error.response?.data || { message: "Login failed" }
+        );
+    }
 });
 
 export const register = createAsyncThunk(
-  'auth/register',
-  async (data, thunkAPI) => {
-    try {
-      const response = await axios.post(
-        'http://15.235.163.93:5000/user/api/create',
-        data
-      );
-      return response.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(
-        error.response?.data || { message: 'SignUp failed' }
-      );
+    "auth/register",
+    async (data, thunkAPI) => {
+        try {
+            const response = await axios.post(
+                "http://15.235.163.93:5000/user/api/create",
+                data
+            );
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(
+                error.response?.data || { message: "SignUp failed" }
+            );
+        }
     }
-  }
 );
 
 const initialState = {
-  user: JSON.parse(localStorage.getItem('user')) || null,
-  authStatus: false,
+    user: JSON.parse(localStorage.getItem("user")) || null,
+    authStatus: false,
 };
 
 const authSlice = createSlice({
-  name: 'auth',
-  initialState,
-  reducers: {
-    setUser(state, action) {
-      state.user = action.payload;
+    name: "auth",
+    initialState,
+    reducers: {
+        setUser(state, action) {
+            state.user = action.payload;
+        },
+        setAuthStatus(state, action) {
+            state.authStatus = action.payload;
+        },
     },
-    setAuthStatus(state, action) {
-      state.authStatus = action.payload;
+    extraReducers(builder) {
+        builder
+            .addCase(register.fulfilled, (state, action) => {
+                console.log(action.payload);
+                state.user = action.payload;
+                state.authStatus = true;
+
+                localStorage.removeItem("user");
+                localStorage.setItem("user", JSON.stringify(action.payload));
+            })
+            .addCase(register.rejected, (state, action) => {
+                console.log(action.payload);
+                const { error, message } = action.payload;
+                alert(error || message || action.payload);
+            })
+            .addCase(login.rejected, (state, action) => {
+                console.log(action.payload);
+                alert(action.payload.error || action.payload);
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                console.log(action.payload);
+                state.user = action.payload;
+                state.authStatus = true;
+
+                localStorage.removeItem("user");
+                localStorage.setItem("user", JSON.stringify(action.payload));
+            });
     },
-  },
-  extraReducers(builder) {
-    builder
-      .addCase(register.fulfilled, (state, action) => {
-        console.log(action.payload);
-        state.user = action.payload;
-        state.authStatus = true;
-
-        localStorage.removeItem('user');
-        localStorage.setItem('user', JSON.stringify(action.payload));
-      })
-      .addCase(register.rejected, (state, action) => {
-        console.log(action.payload);
-        const { error, message } = action.payload;
-        alert(error || message || action.payload);
-      })
-      .addCase(login.rejected, (state, action) => {
-        console.log(action.payload);
-        alert(action.payload.error || action.payload);
-      })
-      .addCase(login.fulfilled, (state, action) => {
-        console.log(action.payload);
-        state.user = action.payload;
-        state.authStatus = true;
-
-        localStorage.removeItem('user');
-        localStorage.setItem('user', JSON.stringify(action.payload));
-      });
-  },
 });
 
 export default authSlice.reducer;
