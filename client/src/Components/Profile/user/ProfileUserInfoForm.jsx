ce93cefa8c3a5529912df934b9aac1a2d0c203eb
Nazmul Hossen Rahul <69730793+NazmulRahul@users.noreply.github.com>
2025-06-14 23:54:29 +0600

    Update ProfileUserInfoForm.jsx

--- client/src/Components/Profile/user/ProfileUserInfoForm.jsx
+++ client/src/Components/Profile/user/ProfileUserInfoForm.jsx
@@ -10,8 +10,8 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { Input } from "@/Components/ui/input";
+import { Label } from "@/Components/ui/label";
 import { Textarea } from "@/Components/ui/textarea";
 import { SquarePen } from "lucide-react";
 import handleFieldUpdate from "./handleFieldUpdate";
